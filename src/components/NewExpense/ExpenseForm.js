930ca1fb0fed7b47390909a58f1afa9398c15bf6
alberto-m-santos <aslms.tic@gmail.com>
2022-02-08 14:30:22 +0000

    add new chart

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,7 +30,6 @@ const ExpenseForm = (props) => {
         setEnteredAmount('');
         setEnteredDate('');
     };
-        
 
         return (
 
@@ -53,6 +52,7 @@ const ExpenseForm = (props) => {
                 </div>
 
                 <div className="new-expense__actions">
+                    <button type='button' onClick={props.onCancel}>Cancel</button>
                     <button type='submit'>Add Expense</button>
                 </div>
             </div>
